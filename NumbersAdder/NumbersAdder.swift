29eda6b8d9cf6ccb25a9ee88f8604322c1a9c8b0
ormenashe <menesh2@gmail.com>
2018-07-08 12:39:47 +0300

    init

---
+++ NumbersAdder/NumbersAdder.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersAdder.swift
+//  NumbersAdder
+//
+//  Created by Or Menashe on 08/07/2018.
+//  Copyright © 2018 Or Menashe. All rights reserved.
+//
+
+import UIKit
+
+class NumbersAdder {
+    
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+
+}
